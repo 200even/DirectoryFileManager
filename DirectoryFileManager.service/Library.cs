d0ce780431d27bd2320ca5f377cf568ea4dd7f56
Scott Ferguson <esfergus@gmail.com>
2015-10-21 15:37:13 -0500

    Initial
    Initial commit with timer.

---
+++ DirectoryFileManager.service/Library.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryFileManager.service
+{
+    public static class Library
+    {
+        public static void WriteErrorLog(Exception ex)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + ex.Message.ToString().Trim());
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ DirectoryFileManager.service/Service1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace DirectoryFileManager.service
+{
+    public partial class Service1 : ServiceBase
+    {
+        private static Timer timer;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Library.WriteErrorLog(new Exception("Directory File Manager started"));
+            timer = new Timer(10000);
+            timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            timer.Enabled = true;     
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            //Project logic TODO
+            //var directory = new DirectoryInfo("C:\\MyDirectory");
+            //var myFile = directory.GetFiles().OrderByDescending(f => f.LastWriteTime).First()
+            Library.WriteErrorLog(new Exception("The job has completed."));
+        }
+
+        protected override void OnStop()
+        {
+            timer.Enabled = false;
+            Library.WriteErrorLog(new Exception("Directory File Manager stopped"));
+        }
+    }
+}
