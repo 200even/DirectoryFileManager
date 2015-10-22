5aabd8beebf07b4cb6ff15d39849319f2efd41a0
Scott Ferguson <esfergus@gmail.com>
2015-10-22 11:49:44 -0500

    Managing one directory for max number of items

--- DirectoryFileManager.service/Service1.cs
+++ DirectoryFileManager.service/Service1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.ServiceProcess;
@@ -20,9 +21,17 @@ namespace DirectoryFileManager.service
             Library.WriteErrorLog(new Exception("Directory File Manager started"));
             scheduledCheck(60000);
             monitorDirectories();
-
         }
 
+        protected override void OnStop()
+        {
+            watcher.EnableRaisingEvents = false;
+            watcher.Dispose();
+            timer.Enabled = false;
+            Library.WriteErrorLog(new Exception("Directory File Manager stopped"));
+        }
+
+        //File watcher events
         private void monitorDirectories()
         {
             watcher = new FileSystemWatcher("C:/Users/esfer_000/Desktop/DirectoryTest");
@@ -36,6 +45,40 @@ namespace DirectoryFileManager.service
             watcher.EnableRaisingEvents = true;
         }
 
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            var directory = new DirectoryInfo("C:/Users/esfer_000/Desktop/DirectoryTest");
+            
+            //Get the files and folders in the directory and order by descending
+            List<FileSystemInfo> items = new List<FileSystemInfo>();           
+            FileSystemInfo[] folders = directory.GetDirectories();
+            FileSystemInfo[] files = directory.GetFiles();
+            foreach(var f in folders)
+            {
+                items.Add(f);
+            }
+            foreach(var f in files)
+            {
+                items.Add(f);
+            }
+            items.OrderByDescending(f => f.LastWriteTime);
+
+            var oldest = items.Last();
+            if (items.Count > 5)
+            {
+                try
+                {
+                    oldest.Delete();
+                }
+                catch
+                {
+                }
+            }
+            Library.WriteErrorLog(new Exception($"The directory has been modified. There are {items.Count} items in the directory."));
+        }
+
+
+        //Timed checking events
         private void scheduledCheck(int interval)
         {
             timer = new Timer(interval);
@@ -43,11 +86,7 @@ namespace DirectoryFileManager.service
             timer.Enabled = true;
         }
 
-        private void OnChanged(object sender, FileSystemEventArgs e)
-        {
-            Library.WriteErrorLog(new Exception("A file has been added."));
-        }
-
+        
         private void OnTimedEvent(object sender, ElapsedEventArgs e)
         {
             //Project logic
@@ -56,12 +95,6 @@ namespace DirectoryFileManager.service
             Library.WriteErrorLog(new Exception("The job has completed."));
         }
 
-        protected override void OnStop()
-        {
-            watcher.EnableRaisingEvents = false;
-            watcher.Dispose();
-            timer.Enabled = false;
-            Library.WriteErrorLog(new Exception("Directory File Manager stopped"));
-        }
+        
     }
 }
